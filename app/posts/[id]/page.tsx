3fbcb8b86825628cd53364c2fd0b6a094d8c30c8
jesse <szx961102@gmail.com>
2022-11-02 00:30:47 +0800

    update

---
+++ app/posts/[id]/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export default async function PostPage({ params }: { params: { id: number }, }) {
+  const post = await getData(params.id);
+  return (
+    <div>
+      <Link href={'/'}>Home</Link>
+      <p className="mb-5 text-2xl font-bold">{post.title}</p>
+      <p>{post.body}</p>
+      <p>{post.body}</p>
+
+    </div>
+  );
+}
+
+async function getData(id: number) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  return res.json();
+}
