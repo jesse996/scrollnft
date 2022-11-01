817d466c31039059506f7444ee173a186211c665
jesse <szx961102@gmail.com>
2022-11-02 01:51:07 +0800

    1

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -1,32 +1,3 @@
-// import Link from "next/link";
-// import { Suspense } from "react";
-
-// export default async function Photo() {
-//   const posts = await getData1();
-
-//   return <div>
-//     <Suspense fallback={<p>Loading feed...</p>}>
-//       <div>
-//         <Link href={'/'} className=' text-2xl'>Home</Link>
-//       </div>
-//       {posts.map((post: any) => (
-//         <div key={post.id}>
-//           <Link href={`/photos/${post.id}`}>
-//             {post.title}
-//           </Link>
-//         </div>
-//       ))}
-//     </Suspense>
-//     photo
-//   </div>
-// }
-
-// async function getData1() {
-//   const res = await fetch('https://jsonplaceholder.typicode.com/photos')
-//   return await res.json()
-// }
-
-
 import Link from "next/link";
 import { Suspense } from "react";
 
