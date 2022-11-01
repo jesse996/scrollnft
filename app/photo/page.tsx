d0525aa3fce8c17969a454644587ae9e1f279947
jesse <szx961102@gmail.com>
2022-11-02 01:17:22 +0800

    update photo

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -5,18 +5,19 @@ export default async function Photo() {
   const posts = await getData();
 
   return <div>
-    <Suspense fallback={<p>Loading feed...</p>}>
+    {/* <Suspense fallback={<p>Loading feed...</p>}>
       <div>
         <Link href={'/'} className=' text-2xl'>Home</Link>
       </div>
       {posts.map((post: any) => (
         <div key={post.id}>
-          {/* <Link href={`/photos/${post.id}`}> */}
+          <Link href={`/photos/${post.id}`}>
           {post.title}
-          {/* </Link> */}
+          </Link>
         </div>
       ))}
-    </Suspense>
+    </Suspense> */}
+    photo
   </div>
 }
 
