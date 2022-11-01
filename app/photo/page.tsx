a60fd16bbb925287f93a6b063fa273c94e82ccf7
jesse <szx961102@gmail.com>
2022-11-02 01:43:34 +0800

    1

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -1,8 +1,37 @@
+// import Link from "next/link";
+// import { Suspense } from "react";
+
+// export default async function Photo() {
+//   const posts = await getData1();
+
+//   return <div>
+//     <Suspense fallback={<p>Loading feed...</p>}>
+//       <div>
+//         <Link href={'/'} className=' text-2xl'>Home</Link>
+//       </div>
+//       {posts.map((post: any) => (
+//         <div key={post.id}>
+//           <Link href={`/photos/${post.id}`}>
+//             {post.title}
+//           </Link>
+//         </div>
+//       ))}
+//     </Suspense>
+//     photo
+//   </div>
+// }
+
+// async function getData1() {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+//   return await res.json()
+// }
+
+
 import Link from "next/link";
 import { Suspense } from "react";
 
-export default async function Photo() {
-  const posts = await getData1();
+export default async function Post() {
+  const posts = await getData();
 
   return <div>
     <Suspense fallback={<p>Loading feed...</p>}>
@@ -11,17 +40,17 @@ export default async function Photo() {
       </div>
       {posts.map((post: any) => (
         <div key={post.id}>
-          <Link href={`/photos/${post.id}`}>
+          <Link href={`/posts/${post.id}`}>
             {post.title}
           </Link>
         </div>
       ))}
+
     </Suspense>
-    photo
   </div>
 }
 
-async function getData1() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/photos')
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
   return await res.json()
 }
