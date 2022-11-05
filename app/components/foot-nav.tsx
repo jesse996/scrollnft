e16fffa25e4d2ab0eb038d6b0ea78e296c8f75c7
jesse <szx961102@gmail.com>
2022-11-05 19:09:41 +0800

    fix usepathname

--- app/components/foot-nav.tsx
+++ app/components/foot-nav.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 import { usePathname } from 'next/navigation';
 
 export default function FootNav() {
-  const pathname: string = usePathname()
+  const pathname = usePathname()
 
   // if (pathname !== '/' && pathname !== '/me') {
   //   return null
